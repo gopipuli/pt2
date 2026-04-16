f8c679d9302d07ad23046bf916d5b88905784c2b
GOPI CHAND <gopicpuli@gmail.com>
2026-04-16 16:32:28 +0530

    add

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -54,12 +54,7 @@ const Navbar = () => {
         ))}
 
 
-        <div className='hidden lg:flex items-center text-sm gap-2 border
-        border-borderColor px-3 rounded-full max-w-56'>
-            <input type="text" className="py-1.5 w-full bg-transparent
-            outline-none placeholder-gray-500" placeholder="Search products"/>
-            <img src={assets.search_icon} alt=" search" />
-        </div>
+         
 
         <div className="flex items-center gap-4">
            <button onClick={()=> isOwner ? navigate('/owner') : changeRole()} 
